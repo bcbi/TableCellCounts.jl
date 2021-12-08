193bffaecdd523c8a2a6a58d8e8a08616957bb2e
Documenter.jl <documenter@juliadocs.github.io>
2021-12-08 23:03:31 +0000

    build based on c74e306

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TableCellCounts","category":"page"},{"location":"#TableCellCounts","page":"Home","title":"TableCellCounts","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TableCellCounts.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TableCellCounts]","category":"page"}]
+}

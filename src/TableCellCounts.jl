a9f70112cb943105a2da34fb1176d1f91c58efc0
Dilum Aluthge <dilum@aluthge.com>
2021-12-08 19:02:44 -0500

    Add some dependencies (#3)

--- src/TableCellCounts.jl
+++ src/TableCellCounts.jl
@@ -1,5 +1,12 @@
 module TableCellCounts
 
+import DataFrames
+import ScientificTypes
+import StatsBase
+import Tables
+
 _hello_world() = "hello world"
+
+include("public.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
-using TableCellCounts
-using Test
+import TableCellCounts
+import Test
+
+import CategoricalArrays
+import DataFrames
+import TypedTables
+
+using Test: @testset, @test, @test_throws
 
 @testset "TableCellCounts.jl" begin
     @test TableCellCounts._hello_world() == "hello world"
